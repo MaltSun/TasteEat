3cee5c7c698f1650c570d2ec8c17c26275a80a33
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-07 16:08:35 +0300

    feat: create firt table in db by midration

---
+++ TasteEatBackend/migrations/20241107123532-create-users-table.js
@@ -0,0 +1,41 @@
+"use strict";
+const { DataTypes } = require('sequelize');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("User", {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      role: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
