91540a55902ea0050b2b3213678c84e647931c85
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-11 17:18:19 +0300

    fix: make adaptive design

--- TasteEatFrontend/src/Components/Footer/Footer.jsx
+++ TasteEatFrontend/src/Components/Footer/Footer.jsx
@@ -11,14 +11,16 @@ const Footer = () => {
           <h3>josefin@mail.com</h3>
         </div>
         <div className="secondBlock">
-          <h2 className="email">Join our mailing list for updates, Get news & offers events</h2>
-          <div>
-            <input ></input>
-            <button className="accent buttonText">Subscribe</button>
+          <h2 className="email">
+            Join our mailing list for updates, Get news & offers events
+          </h2>
+          <div className="emailInput">
+            <input className="reviewText" placeholder="Write An E-mail" type="text"></input>
+            <button className="contoureButton">Subscribe</button>
           </div>
         </div>
         <div className="thirdBlock">
-          <h3 >Mon – Fri: 7.00am – 6.00pm</h3>
+          <h3>Mon – Fri: 7.00am – 6.00pm</h3>
           <h3>Sat: 7.00am – 6.00pm</h3>
           <h3>Sun: 8.00am – 6.00pm</h3>
         </div>
--- TasteEatFrontend/src/Components/Header/Header.jsx
+++ TasteEatFrontend/src/Components/Header/Header.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import "./Header.css"
-
+import "./Header.css";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="headerSection">
       <div className="logoSection">
         <button className="contoureButton">Call:8022 96-69-66 </button>
-        <img className="logo"
-          src=".\Images\Logo.jpg"
-          alt="logo"
-        />
-        <button className="filleadButton">Go to profile</button>
+        <img className="logo" src=".\Images\Logo.jpg" alt="logo" />
+       <Link className="filleadButton" to ={{ pathname: "/profile"}}>Go To Profile</Link>
       </div>
       <div className="line"></div>
-      <div className="menuSection">
+     
         <ul className="menuList">
-            <p className="headerP">Main</p>
-            <p className="headerP">Menu</p>
-            <p className="headerP">Order</p>
-            <p className="headerP">Profile</p>
+          <Link className="headerP" to={{ pathname: "/" }}>
+            Main
+          </Link>
+          <Link className="headerP" to={{ pathname: "/menu" }}>
+            Menu
+          </Link>
+          <Link className="headerP" to={{ pathname: "/cart" }}>
+           Cart
+          </Link>
+          <Link className="headerP" to={{ pathname: "/profile" }}>
+           Profile
+          </Link>
+          
         </ul>
-      </div>
     </div>
   );
 };
