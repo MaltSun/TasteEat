cb80af30bd76a4418b270a1cbfe2a8bef4c60c96
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-11 17:21:19 +0300

    fix: change fetch request

--- TasteEatFrontend/src/Components/MenuCard/MenuCard.jsx
+++ TasteEatFrontend/src/Components/MenuCard/MenuCard.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom"; // Импортируйте useHistory для навигации
+import { useNavigate } from "react-router-dom";
 import "./MenuCard.css";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
-import ArtTrackIcon from "@mui/icons-material/ArtTrack";
-import GridViewIcon from "@mui/icons-material/GridView";
 
 const MenuCategory = ({ title, items, isHorizontal }) => {
   const [visibleItems, setVisibleItems] = useState(4);
 
   const handleMoreClick = () => {
-    setVisibleItems((prev) => prev + 4); // Увеличиваем количество отображаемых позиций
+    setVisibleItems((prev) => prev + 4);
   };
 
   const handleHideClick = () => {
-    setVisibleItems((prev) => Math.max(prev - 4, 4)); // Уменьшаем количество до 4
+    setVisibleItems((prev) => Math.max(prev - 4, 4));
   };
 
   return (
-    <div className="menuBlock">
+    <div>
       <h2 className="titleUnder">{title}</h2>
       <div className="menuCategory">
         {items.slice(0, visibleItems).map((item) => (
@@ -33,8 +31,8 @@ const MenuCategory = ({ title, items, isHorizontal }) => {
             <img className="menuPhoto" src={item.photo} alt={item.name} />
             <div>
               <h3 className="title">{item.name}</h3>
-              <p>{item.description}</p>
-              <h3>{item.price}</h3>
+              <p className="description">{item.description}</p>
+              <h3>${item.price}</h3>
             </div>
           </div>
         ))}
@@ -53,16 +51,15 @@ const MenuCategory = ({ title, items, isHorizontal }) => {
   );
 };
 
-const MenuCard = () => {
+const MenuCard = ({ isHorizontal, setIsHorizontal }) => {
   const [data, setData] = useState([]);
   const [starters, setStarters] = useState([]);
   const [mains, setMains] = useState([]);
   const [drinks, setDrinks] = useState([]);
   const [desserts, setDesserts] = useState([]);
-  const [isHorizontal, setIsHorizontal] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/dishes") // Запрос к вашему API
+    fetch("http://localhost:3000/api/dish/resource")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -81,17 +78,6 @@ const MenuCard = () => {
 
   return (
     <div className="menuCard">
-      <div className="position">
-        <ArtTrackIcon
-          onClick={() => setIsHorizontal(true)}
-          style={{ cursor: "pointer", marginRight: "10px" }}
-        />
-        <GridViewIcon
-          onClick={() => setIsHorizontal(false)}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-
       <MenuCategory
         title="Starters"
         items={starters}
--- TasteEatFrontend/src/Pages/MenuPage/MenuPage.jsx
+++ TasteEatFrontend/src/Pages/MenuPage/MenuPage.jsx
@@ -1,15 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MenuPage.css";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import SaleCard from "../../Components/SaleCard/SaleCard";
 import MenuCard from "../../Components/MenuCard/MenuCard";
+import ReviewCard from "../../Components/ReviewCard/ReviewCard";
+import CommentCard from "../../Components/CommentCard/CommentCard";
+import ArtTrackIcon from "@mui/icons-material/ArtTrack";
+import GridViewIcon from "@mui/icons-material/GridView";
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Select,
+  MenuItem,
+  Slider,
+  Typography,
+} from "@mui/material";
+import ViewListIcon from "@mui/icons-material/ViewList";
+import ViewModuleIcon from "@mui/icons-material/ViewModule";
+
 const MenuPage = () => {
+  const [isHorizontal, setIsHorizontal] = useState(false);
+  const [view, setView] = useState("list");
+  const [category, setCategory] = useState("");
+  const [priceRange, setPriceRange] = useState([0, 100]); // Предполагаем, что цена от 0 до 100
+
+  const handleChangeView = (event, newView) => {
+    if (newView !== null) {
+      setView(newView);
+      setIsHorizontal(newView === "module");
+    }
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handlePriceChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
+
   return (
     <div>
       <Header />
       <SaleCard />
-      <MenuCard />
+
+      <div className="position">
+        <ArtTrackIcon
+          onClick={() => setIsHorizontal(true)}
+          style={{ cursor: "pointer", marginRight: "10px" }}
+        />
+        <GridViewIcon
+          onClick={() => setIsHorizontal(false)}
+          style={{ cursor: "pointer" }}
+        />
+
+        <Select
+          value={category}
+          onChange={handleCategoryChange}
+          displayEmpty
+          style={{ marginLeft: "10px", minWidth: "120px" }}
+        >
+          <MenuItem value="">
+            <em>All Categories</em>
+          </MenuItem>
+          <MenuItem value="starter">Starters</MenuItem>
+          <MenuItem value="main">Main Dishes</MenuItem>
+          <MenuItem value="drinks">Drinks</MenuItem>
+          <MenuItem value="dessert">Desserts</MenuItem>
+          <MenuItem value="price">
+            <Typography gutterBottom>
+              Price Range: ${priceRange[0]} - ${priceRange[1]}
+            </Typography>
+            <Slider
+              value={priceRange}
+              onChange={handlePriceChange}
+              valueLabelDisplay="auto"
+              min={0}
+              max={100} // Измените максимальное значение на нужное вам
+              style={{ width: "300px", marginLeft: "20px" }}
+            />
+          </MenuItem>
+        </Select>
+      </div>
+
+      <MenuCard
+        isHorizontal={isHorizontal}
+        setIsHorizontal={setIsHorizontal}
+        category={category}
+        priceRange={priceRange}
+      />
+      <section className="reviewSection">
+        <div>
+          <h1>Our Clients Say</h1>
+          <p className="littleLight">
+            We love to hear from customers, so please leave a comment or say
+            hello in an email.
+          </p>
+        </div>
+        <ReviewCard />
+        <CommentCard />
+      </section>
       <Footer />
     </div>
   );
