1e7942bdc795a200a6c7d48629359dd10abe053a
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-11 17:18:56 +0300

    feat: create file for profiles

---
+++ TasteEatFrontend/src/Pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import ProfileCard from "../../Components/ProfileComponent/ProfileComponent";
+const ProfilePage = () => {
+  return (
+    <div>
+      <Header />
+      <ProfileCard />
+      <Footer />
+    </div>
+  );
+};
+
+export default ProfilePage;
