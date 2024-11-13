0a7e4776e51d3b3ecf5464f21d5fbda876f33ddf
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-13 10:54:02 +0300

    feat: add order component for page

--- TasteEatFrontend/src/Pages/ProfilePage/ProfilePage.jsx
+++ TasteEatFrontend/src/Pages/ProfilePage/ProfilePage.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import ProfileCard from "../../Components/ProfileComponent/ProfileComponent";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import OrderComponent from "../../Components/OrderComponent/OrderComponent";
+//import OldOrderComponent from "../../Components/OldOrderComponent/OldOrderComponent";
+import "./ProfilePage.css"
 const ProfilePage = () => {
   return (
-    <div>
+    <div className="profile">
       <Header />
       <ProfileCard />
+      <div className="delivery">
+        <h1>Delivery</h1>
+        <OrderComponent />
+      </div>
+      <div>
+        <p>Прошлые заказы</p>
+        <ArrowDropDownIcon />
+      </div>
+      {/* <OldOrderComponent /> */}
       <Footer />
     </div>
   );
