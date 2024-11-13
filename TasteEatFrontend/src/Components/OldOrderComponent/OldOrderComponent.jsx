78e2c4c70d0dffc512d6f0df8976d78b22c12632
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-13 10:53:38 +0300

    feat: create component for order that completed

---
+++ TasteEatFrontend/src/Components/OldOrderComponent/OldOrderComponent.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const OldOrderComponent = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <h1>номер закааза</h1>
+            <p>№2345</p>
+          </div>
+          <div>
+            <h2>дата оформления</h2>
+            <p>12.11.2023</p>
+          </div>
+          <div>
+            <h2>Дата выполнения</h2>
+            <p>13.11.2023</p>
+          </div>
+          <div>
+            <h2>адресс доставки</h2>
+            <p>лвиырв иморыиморв ыимиыом</p>
+          </div>
+          <div>
+            <h2>выполнил</h2>
+            <p>cgggcs sjc </p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <button>Получить чек </button>
+    </div>
+  );
+};
+
+export default OldOrderComponent;
