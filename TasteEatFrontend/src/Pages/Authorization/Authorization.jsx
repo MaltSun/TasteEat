abe47d547c3f7b40fdfd5ab3b5bc28ba0446b5c8
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-16 13:56:26 +0300

    feat: create form for authorization

---
+++ TasteEatFrontend/src/Pages/Authorization/Authorization.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCredentials } from "../../Store/authSlice";
+import { useNavigate } from "react-router-dom";
+
+const Authorization = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const email = useSelector((state) => state.auth.email);
+  const password = useSelector((state) => state.auth.password);
+
+  const handleAuthorization = async () => {
+    const response = await fetch(
+      "http://localhost:3000/api/authorization/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+
+    const data = await response.json();
+    if (!response.ok) {
+      alert(data.error || "Authorization failed");
+    } else {
+      console.log("Authorization successful!");
+      const { id, role } = data;
+      dispatch(setCredentials({ email, password, userId: id, role }));
+      if (role === "customer") {
+        navigate("/profile");
+      } else if (role === "deliverer") {
+        navigate("/delivery");
+      } else {
+        navigate("/admin");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <label>E-mail</label>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) =>
+          dispatch(setCredentials({ email: e.target.value, password }))
+        }
+        placeholder="Input Your E-mail"
+      />
+
+      <label>Password</label>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) =>
+          dispatch(setCredentials({ email, password: e.target.value }))
+        }
+        placeholder="Enter A password"
+      />
+      <button onClick={handleAuthorization}>Send</button>
+    </div>
+  );
+};
+
+export default Authorization;
