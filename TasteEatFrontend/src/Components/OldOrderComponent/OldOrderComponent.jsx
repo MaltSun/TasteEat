fe7248c49cba3cffa57a9e70b8990133a5172f9b
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-16 14:00:01 +0300

    feat: create card to display current and completed orders

--- TasteEatFrontend/src/Components/OldOrderComponent/OldOrderComponent.jsx
+++ TasteEatFrontend/src/Components/OldOrderComponent/OldOrderComponent.jsx
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./OldOrderComponent.css";
+const OldOrderComponent = ({ customerId }) => {
+  const [orders, setOrders] = useState([]);
 
-const OldOrderComponent = () => {
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/order/completed/customer/2`
+        );
+        if (!response.ok) {
+          throw new Error("Ошибка при получении заказа");
+        }
+        const data = await response.json();
+        setOrders(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchDetails();
+  }, [customerId]);
+
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <h1>номер закааза</h1>
-            <p>№2345</p>
+    <div className="oldOrderComponent">
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <div className="oldCard" key={order.id}>
+            <div>
+              <h1>Order №{order.id}</h1>
+              <p>Issue at: {new Date(order.createdAt).toLocaleString()}</p>
+              <p>Completed at: {new Date(order.updatedAt).toLocaleString()}</p>
+              <p>Address: {order.address}</p>
+            </div>
+            <div className="check">
+              <h2>Complete: {order.deliveryId || "Неизвестно"}</h2>
+
+              <button className="filleadButton">Get A Check</button>
+            </div>
           </div>
-          <div>
-            <h2>дата оформления</h2>
-            <p>12.11.2023</p>
-          </div>
-          <div>
-            <h2>Дата выполнения</h2>
-            <p>13.11.2023</p>
-          </div>
-          <div>
-            <h2>адресс доставки</h2>
-            <p>лвиырв иморыиморв ыимиыом</p>
-          </div>
-          <div>
-            <h2>выполнил</h2>
-            <p>cgggcs sjc </p>
-          </div>
+        ))
+      ) : (
+        <div className="noOrders">
+          <img src="./Images/ProfileImage.png"></img>
+          <h1>No Completed Orders Yet</h1>
         </div>
-      </div>
-      <hr />
-      <button>Получить чек </button>
+      )}
     </div>
   );
 };
