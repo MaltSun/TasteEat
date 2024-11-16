6993dae458850433e434726de4054442c6393d78
Polina Solodkova <polina.solodkova@gmail.com>
2024-11-16 13:54:30 +0300

    feat: complete profile for user

--- TasteEatFrontend/src/Pages/ProfilePage/ProfilePage.jsx
+++ TasteEatFrontend/src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import ProfileCard from "../../Components/ProfileComponent/ProfileComponent";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import OrderComponent from "../../Components/OrderComponent/OrderComponent";
-//import OldOrderComponent from "../../Components/OldOrderComponent/OldOrderComponent";
-import "./ProfilePage.css"
+import OldOrderComponent from "../../Components/OldOrderComponent/OldOrderComponent";
+import "./ProfilePage.css";
+
 const ProfilePage = () => {
+  const [showOldOrders, setShowOldOrders] = useState(false);
+
+  const toggleOldOrders = () => {
+    setShowOldOrders((prev) => !prev);
+  };
+
   return (
     <div className="profile">
       <Header />
@@ -15,11 +22,15 @@ const ProfilePage = () => {
         <h1>Delivery</h1>
         <OrderComponent />
       </div>
-      <div>
-        <p>Прошлые заказы</p>
-        <ArrowDropDownIcon />
+      <div className="oldOrder">
+        <h1>Прошлые заказы</h1>
+        <ArrowDropDownIcon fontSize="large" onClick={toggleOldOrders}  />
       </div>
-      {/* <OldOrderComponent /> */}
+      {showOldOrders && (
+        <div id="oldOrder">
+          <OldOrderComponent />
+        </div>
+      )}
       <Footer />
     </div>
   );
