dba40a498a3265066e45a864b2b4483bac791eca
Polina Solodkova <polina.solodkova@gmail.com>
2024-12-11 09:06:02 +0300

    feat: component for creation dish

--- TasteEatFrontend/src/Components/CreateDish/CreateDish.jsx
+++ TasteEatFrontend/src/Components/CreateDish/CreateDish.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
+import "./CreateDish.css"; // Убедитесь, что у вас есть правильный путь к стилям
 
-const CreateDish = () => {
+const CreateDish = ({ onDishCreated, onClose }) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
   const [weight, setWeight] = useState("");
   const [price, setPrice] = useState("");
   const [photo, setPhoto] = useState("");
-  const [calories, setCalories] = useState("");
-  const [fat, setFat] = useState("");
-  const [protein, setProtein] = useState("");
-  const [carbs, setCarbs] = useState("");
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
@@ -24,10 +21,6 @@ const CreateDish = () => {
       weight,
       price,
       photo,
-      calories,
-      fat,
-      protein,
-      carbs,
     };
 
     try {
@@ -46,71 +39,87 @@ const CreateDish = () => {
       const newDish = await response.json();
       setSuccess(`Блюдо "${newDish.name}" успешно создано!`);
       setError(null);
-      setName("");
-      setDescription("");
-      setCategory("");
-      setWeight("");
-      setPrice("");
-      setPhoto("");
-      setCalories("");
-      setFat("");
-      setProtein("");
-      setCarbs("");
-      onDishCreated(); 
+      resetForm();
+      onDishCreated();
     } catch (error) {
       setError(error.message);
       setSuccess(null);
     }
   };
 
+  const resetForm = () => {
+    setName("");
+    setDescription("");
+    setCategory("");
+    setWeight("");
+    setPrice("");
+    setPhoto("");
+    onClose(); // Закрыть попап
+  };
+
   return (
-    <div>
-      <h2>Создать новое блюдо</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Название:</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
-        </div>
-        <div>
-          <label>Описание:</label>
-          <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
-        </div>
-        <div>
-          <label>Категория:</label>
-          <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} required />
-        </div>
-        <div>
-          <label>Вес:</label>
-          <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} required />
-        </div>
-        <div>
-          <label>Цена:</label>
-          <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required />
-        </div>
-        <div>
-          <label>Фото (URL):</label>
-          <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} required />
-        </div>
-        <div>
-          <label>Калории:</label>
-          <input type="number" value={calories} onChange={(e) => setCalories(e.target.value)} required />
-        </div>
-        <div>
-          <label>Жиры:</label>
-          <input type="number" value={fat} onChange={(e) => setFat(e.target.value)} required />
-        </div>
-        <div>
-          <label>Белки:</label>
-          <input type="number" value={protein} onChange={(e) => setProtein(e.target.value)} required />
-        </div>
-        <div>
-          <label>Углеводы:</label>
-          <input type="number" value={carbs} onChange={(e) => setCarbs(e.target.value)} required />
-        </div>
-        <button type="submit">Создать блюдо</button>
-      </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {success && <p style={{ color: "green" }}>{success}</p>}
+    <div className="popup">
+      <div className="popup-content">
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <h2>New Dish</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Description:</label>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Category:</label>
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            >
+              <option value="">Choose One</option>
+              <option value="main">Main Dish</option>
+              <option value="starter">Starter</option>
+              <option value="drinks">Drink</option>
+              <option value="dessert">Dessert</option>
+            </select>
+          </div>
+
+          <div>
+            <label>Price:</label>
+            <input
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Photo (URL):</label>
+            <input
+              type="text"
+              value={photo}
+              onChange={(e) => setPhoto(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Create</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {success && <p style={{ color: "green" }}>{success}</p>}
+      </div>
     </div>
   );
 };
