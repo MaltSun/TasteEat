2ea1c56265c00f18afce4c9de13cdcf9e87b79ec
Polina Solodkova <polina.solodkova@gmail.com>
2024-12-11 08:55:07 +0300

    fix: change margin for lanterns

--- TasteEatFrontend/src/Pages/MainPage/MainPage.jsx
+++ TasteEatFrontend/src/Pages/MainPage/MainPage.jsx
@@ -24,73 +24,66 @@ const MainPage = () => {
   }, [dispatch]);
 
   return (
-      <div className="mainPage">
-        <Header />
+    <div className="mainPage">
+      <Header />
 
-        <section className="mainSection">
-          <div className="mainText">
-              <h1 className="mainH">Welcome To Restaurant</h1>
-          
-              <p className="mainP">
-                The people, food and the prime locations make the perfect place
-                for good friends & family to come together and have a great time.
-              </p>
-            
-            <button className="contoureButton">
-              <Link to={{ pathname: "/menu" }}>View Menu</Link>
-            </button>
-          </div>
-            <img src="./Images/MainPhoto.png" alt="Main" />
-       
-        </section>
+      <section className="mainSection">
+        <div className="mainText">
+          <h1 className="mainH">Welcome To Restaurant</h1>
 
-        <ThesisCard />
-        
-        <section className="informationSection">
-      
-            <img src="./Images/cafePage.png" alt="Cafe" />
-         
-          <div className="deliciousStory">
-            <div>
-              <h2>The Delicious Story</h2>
-              <p>
-                The people, food and the prime locations make the perfect place
-                for friends & family to come together and have a great time.
-              </p>
-            </div>
-            <div className="blockYear">
-              <div className="year">
-                <h2>2018</h2>
-                <p>Plan for this restaurant to deliver healthy food.</p>
-              </div>
-              <div className="year">
-                <h2>2020</h2>
-                <p>Happily in the fourth year by fulfilling the motto.</p>
-              </div>
-            </div>
-          </div>
-        </section>
+          <p className="mainP">
+            The people, food and the prime locations make the perfect place for
+            good friends & family to come together and have a great time.
+          </p>
 
-       
-          <img
-            className="lanterns"
-            src="./Images/lanterns.png"
-            alt="Lanterns"
-          />
+          <button className="contoureButton">
+            <Link to={{ pathname: "/menu" }}>View Menu</Link>
+          </button>
+        </div>
+        <img src="./Images/MainPhoto.png" alt="Main" />
+      </section>
 
-        <section className="offerSection">
-          <div className="offerText">
-            <h4>What we offer</h4>
-            <h2>Our Great Services</h2>
+      <ThesisCard />
+
+      <section className="informationSection">
+        <img src="./Images/cafePage.png" alt="Cafe" />
+
+        <div className="deliciousStory">
+          <div>
+            <h2>The Delicious Story</h2>
             <p>
-              The atmosphere sets the stage. It’s about more than just a dining
-              room away from your home. Food takes the spotlight as guests.
+              The people, food and the prime locations make the perfect place
+              for friends & family to come together and have a great time.
             </p>
           </div>
-          <OfferCard />
-        </section>
-        <Footer />
-      </div>
+          <div className="blockYear">
+            <div className="year">
+              <h2>2018</h2>
+              <p>Plan for this restaurant to deliver healthy food.</p>
+            </div>
+            <div className="year">
+              <h2>2020</h2>
+              <p>Happily in the fourth year by fulfilling the motto.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <img className="lanterns" src="./Images/lanterns.png" alt="Lanterns" />
+
+      <section className="offerSection">
+        <div className="offerText">
+          <h4>What we offer</h4>
+          <h2>Our Great Services</h2>
+          <p>
+            The atmosphere sets the stage. It’s about more than just a dining
+            room away from your home. Food takes the spotlight as guests.
+          </p>
+        </div>
+        <OfferCard />
+      </section>
+      <Footer />
+    </div>
   );
 };
 
