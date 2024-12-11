edd49d4c3f90f34781204e5e88ef8ad9fd3e804a
Polina Solodkova <polina.solodkova@gmail.com>
2024-12-11 08:53:17 +0300

    feat: create error page

--- TasteEatFrontend/src/App.jsx
+++ TasteEatFrontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import MainPage from "./Pages/MainPage/MainPage";
-import {  useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MenuPage from "./Pages/MenuPage/MenuPage";
 import OrderPage from "./Pages/OrderPage/OrderPage";
@@ -10,11 +10,10 @@ import AdminPage from "./Pages/AdminPage/AdminPage";
 import CartPage from "./Pages/CartPage/CartPage";
 import Registration from "./Pages/Registration/Registration";
 import Authorization from "./Pages/Authorization/Authorization";
-import { clearCredentials } from './Store/authStore';
-
+import { clearCredentials } from "./Store/authStore";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 function App() {
-  const [count, setCount] = useState(0);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -30,21 +29,23 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/order" element={<OrderPage />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/reg" element={<Registration />} />
+        <Route path="/login" element={<Authorization />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route element={<PrivateRoute allowedRoles={["delivery"]} />}>
           <Route path="/delivery" element={<DeliveryPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/reg" element={<Registration />} />
-          <Route path="/login" element={<Authorization />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        </Route>
+        <Route element={<PrivateRoute allowedRoles={["user"]} />}></Route>
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}></Route>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
